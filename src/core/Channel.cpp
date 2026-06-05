fe676b5d3aebff17785ff4832e85006f68cc2805
Ricardo Moreira <devrmoreira@gmail.com>
2026-06-05 12:23:11 +0100

    added setters & getters + isMember function to find a client

--- src/core/Channel.cpp
+++ src/core/Channel.cpp
@@ -7,24 +7,71 @@ Channel::Channel(const std::string& name) : _name(name)
 
 void Channel::addOperator(Client& client)
 {
-	_clients.push_back(Member(&client, true));
+	_members.push_back(Member(&client, true));
 }
 
 void Channel::addClient(Client& client)
 {
-	_clients.push_back(Member(&client, false));
+	_members.push_back(Member(&client, false));
 }
 
-void Channel::setTopic(const std::string& topic)
+void Channel::setOperator(const std::string& nick, bool val)
 {
-	_topic = topic;
+	for(size_t i = 0; i < _members.size(); i++)
+	{
+		if(_members[i].client->getNickname() == nick)
+		{
+			_members[i].isOperator = val;
+			return ;
+		}
+	}
+}
+
+void Channel::setTopic(const std::string& str)
+{
+	_topic = str;
 }
 
 void Channel::broadcast(Client& sender, const std::string& msg)
 {
-	for(size_t i = 0; i < _clients.size(); i++)
+	for(size_t i = 0; i < _members.size(); i++)
 	{
-		if(_clients[i].client != &sender)
-			_clients[i].client->sendMessageToClient(msg);
+		if(_members[i].client != &sender)
+			_members[i].client->sendMessageToClient(msg);
 	}
+}
+
+bool Channel::isMember(const std::string& nick) const
+{
+	for(size_t i = 0; i < _members.size(); i++)
+	{
+		if(_members[i].client->getNickname() == nick)
+			return true;
+	}
+
+	return false;
+}
+
+void Channel::setKey(const std::string& str)
+{
+	_key = str;
+}
+void Channel::setUserLimit(unsigned int n)
+{
+	if(n > 0)
+		_userLimit = n;
+}
+
+void Channel::setInviteOnly(const bool val)
+{
+	_inviteOnly = val;
+}
+void Channel::setTopicProtected(const bool val)
+{
+	_topicProtected = val;
+}
+
+std::string Channel::getName() const
+{
+	return _name;
 }
